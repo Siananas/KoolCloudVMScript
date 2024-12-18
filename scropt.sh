83a51ccbec90ea6990c38e76275bd3d79aba9f07
Siananas <91467624+Siananas@users.noreply.github.com>
2024-12-18 15:00:54 +0100

    Update scropt.sh

--- scropt.sh
+++ scropt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update
+sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
